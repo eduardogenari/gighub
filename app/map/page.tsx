3974b2adc1c05cd0fc3f4262201e5a6fc5d7a315
Alba Vilanova <alba.vilanova@bsc.es>
2024-07-20 15:35:30 +0200

    Move map from home to map route

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import dynamic from 'next/dynamic';
+import React, { useMemo } from 'react'
+import Filters from "@/components/filters";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
 
-export default function page() {
+export default function Page() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/components/Map"), {
+        loading: () => <p>"Loading"</p>,
+        ssr: false,
+      }),
+    []
+  );
+
   return (
-    <div>
-      <h1>map page</h1>
-    </div>
-  )
+    <main className="h-screen w-screen flex overflow-hidden bg-gray-200">
+
+      <div className="w-1/5 bg-white p-4">
+        <h1 className="text-lg font-bold mb-4">Filters</h1>
+        <NavigationMenu></NavigationMenu>
+        <Filters />
+      </div>
+
+      <div className="w-4/5 bg-gray-100">
+        <Map />
+      </div>
+
+    </main>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
 import Filters from "@/components/filters";
 import { NavigationMenu } from "@/components/ui/navigation-menu";
-import { useMemo } from "react";
-import dynamic from "next/dynamic";
+import Image from "next/image";
 
 export default function Home() {
-  const Map = useMemo(
-    () =>
-      dynamic(() => import("@/components/Map"), {
-        loading: () => <p>"Loading"</p>,
-        ssr: false,
-      }),
-    []
-  );
-
   return (
     <main className="h-screen w-screen flex overflow-hidden bg-gray-200">
+
 
       <div className="w-1/5 bg-white p-4">
         <h1 className="text-lg font-bold mb-4">Filters</h1>
@@ -22,8 +13,8 @@ export default function Home() {
         <Filters />
       </div>
 
-      <div className="w-4/5 bg-gray-100">
-        <Map />
+      <div className="w-4/5 bg-gray-100 p-4">
+        <h1 className="text-lg font-bold mb-4">Map</h1>
       </div>
 
     </main>
