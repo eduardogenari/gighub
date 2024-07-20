21875c870b8913c6933338cc9266721d55d7fc38
Alba Vilanova <alba.vilanova@bsc.es>
2024-07-20 19:24:56 +0200

    Add calendar filters

--- components/filters.tsx
+++ components/filters.tsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import { Button } from './ui/button'
-import { Input } from './ui/input'
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "./ui/button";
+import { Form } from "@/components/ui/form";
+import CalendarItem from "./CalendarItem";
+
+const FormSchema = z.object({
+  startDate: z.date({
+    required_error: "A start date is required.",
+  }),
+  endDate: z.date({
+    required_error: "An end date is required.",
+  }),
+});
 
 export default function Filters() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(JSON.stringify(data, null, 2));
+  }
+
   return (
-    <div className="flex flex-col gap-2">
-        <Input placeholder='Country'></Input>
-        <Button>Apply</Button>
-    </div>
-  )
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <CalendarItem
+          form={form}
+          name={"startDate"}
+          label={"Start date"}
+          placeholder={"Pick a start date"}
+        />
+        <CalendarItem
+          form={form}
+          name={"endDate"}
+          label={"End date"}
+          placeholder={"Pick an end date"}
+        />
+        <Button type="submit">Apply</Button>
+      </form>
+    </Form>
+  );
 }
