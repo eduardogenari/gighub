08a88cd752ace8ce5f9587f50ce108cb2869c195
Alba Vilanova <alba.vilanova@outlook.com>
2024-07-23 21:02:58 +0200

    Format

--- app/concerts/page.tsx
+++ app/concerts/page.tsx
@@ -1,29 +1,32 @@
 import { actionGetAllEvents } from "@/actions/events";
 import { Event, getAllEvents } from "@/lib/concerts";
 
-
-
-
 export default async function Page() {
- // let concerts: Event[] = [];
+  // let concerts: Event[] = [];
   const concerts = await actionGetAllEvents();
- 
+
   return (
     <main className="p-6">
       {concerts.map((concert) => (
         <div key={concert.id}>
-
-<h2>{concert.name}</h2>
-      <img src={concert.images.find((img) => img.width === 305 && img.ratio === "4_3")?.url} alt={concert.name} />
+          <h2>{concert.name}</h2>
+          <img
+            src={
+              concert.images.find(
+                (img) => img.width === 305 && img.ratio === "4_3"
+              )?.url
+            }
+            alt={concert.name}
+          />
           <p>{`Date: ${concert.dates.start.localDate}`}</p>
           <p>{`Time: ${concert.dates.start.localTime}`}</p>
-    
+
           {concert.venues.map((venue) => (
             <div key={venue.id}>
               <h3>{venue.name}</h3>
               <p>{`Location: ${venue.location.latitude}, ${venue.location.longitude}`}</p>
               <p>{`Address: ${venue.address}`}</p>
-              </div>
+            </div>
           ))}
         </div>
       ))}
