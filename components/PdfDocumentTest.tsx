2648030385ae479f74ecd6ebb461779deb941ca6
eduardogenari <emgenari@gmail.com>
2024-08-31 14:53:32 +0200

    pdf design

--- components/PdfDocumentTest.tsx
+++ components/PdfDocumentTest.tsx
@@ -6,23 +6,61 @@ import {
   View,
   StyleSheet,
   Image,
+  Font,
 } from "@react-pdf/renderer";
 import { Event } from "@/types/event";
+
+Font.register({
+  family: "Open Sans",
+  fonts: [
+    {
+      src: "https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-regular.ttf",
+    },
+    {
+      src: "https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-600.ttf",
+      fontWeight: 600,
+    },
+  ],
+});
 
 const stylesPDF = StyleSheet.create({
   page: {
     flexDirection: "column",
-    padding: 30,
-    backgroundColor: "blue",
+    padding: 10,
   },
   section: {
     margin: 10,
     padding: 10,
-    backgroundColor: "red",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  left: {
+    width: "215px",
+    paddingRight: 1,
+  },
+  text: {
+    fontSize: 6,
+    fontFamily: "Open Sans",
+    marginBottom: 5,
   },
   title: {
-    fontSize: 24,
-    marginBottom: 20,
+    fontSize: 8,
+    fontFamily: "Open Sans",
+    fontWeight: 600,
+    marginBottom: 10,
+  },
+  image: {
+    width: 500,
+    aspectRatio: "3/2",
+  },
+  barCode: {
+    width: 500,
+    height: 25,
+  },
+  line: {
+    margin: 0,
+    padding: 0,
+    borderTop: "1px solid black",
   },
 });
 
@@ -33,27 +71,50 @@ type PdfDocumentTestProps = {
 const PdfDocumentTest: React.FC<PdfDocumentTestProps> = ({ event }) => (
   <Document>
     <Page size="A4" style={stylesPDF.page}>
+      <View style={stylesPDF.line}></View>
       <View style={stylesPDF.section}>
-        <Image src={event.image[0].url} />
-        <Text style={stylesPDF.title}>Event Details: {event.name}</Text>
-        <Text>Date: {new Date(event.startDate).toLocaleDateString()}</Text>
-        <Text>
-          Artists: {event.artist.map((artist) => artist.name).join(", ")}
-        </Text>
-        <Text>
-          Venue:{" "}
-          {event.venue
-            .map((venue) => `${venue.name}, ${venue.address}`)
-            .join(", ")}
-        </Text>
-        <Text>Genre: {event.genre.join(", ")}</Text>
-        <Text>
-          Price:{" "}
-          {event.priceRange
-            .filter((priceRange) => priceRange.type == "standard")
-            .map((priceRange) => `${priceRange.min} ${priceRange.currency}`)
-            .join(", ")}
-        </Text>
+        <View style={stylesPDF.left}>
+          <Text style={stylesPDF.title}>gighub</Text>
+        </View>
+        <Image
+          style={stylesPDF.barCode}
+          src="../gighub/public/images/bar.png"
+        />
+      </View>
+      <View style={stylesPDF.line}></View>
+      <View style={stylesPDF.section}>
+        <View style={stylesPDF.left}>
+          <Text style={stylesPDF.title}>{event.name}</Text>
+          <Text style={stylesPDF.text}>
+            Date: {new Date(event.startDate).toLocaleDateString()}
+          </Text>
+          <Text style={stylesPDF.text}>
+            Artists: {event.artist.map((artist) => artist.name).join(", ")}
+          </Text>
+          <Text style={stylesPDF.text}>
+            Venue:{" "}
+            {event.venue
+              .map((venue) => `${venue.name}, ${venue.address}`)
+              .join(", ")}
+          </Text>
+          <Text style={stylesPDF.text}>Genre: {event.genre.join(", ")}</Text>
+          <Text style={stylesPDF.text}>
+            Price:{" "}
+            {event.priceRange
+              .filter((priceRange) => priceRange.type == "standard")
+              .map((priceRange) => `${priceRange.min} ${priceRange.currency}`)
+              .join(", ")}
+          </Text>
+        </View>
+        <Image style={stylesPDF.image} src={event.image[0].url} />
+      </View>
+      <View style={stylesPDF.line}></View>
+      <View style={stylesPDF.section}>
+        <View style={stylesPDF.left}>
+          <Text style={stylesPDF.text}>
+            * Ticket not valid. This is a student project.
+          </Text>
+        </View>
       </View>
     </Page>
   </Document>
